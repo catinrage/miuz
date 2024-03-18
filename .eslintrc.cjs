6a60ee0862a927d77ac4cd447485c93c1889def4
catinrage <catinrage@gmail.com>
2024-03-19 00:06:20 +0330

    Add state machine classes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,11 @@
 /** @type { import("eslint").Linter.Config } */
 module.exports = {
   root: true,
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
-  rules: {
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-unused-vars": "off"
-  },
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   parserOptions: {
-    sourceType: "module",
+    sourceType: 'module',
     ecmaVersion: 2020
   },
   env: {
